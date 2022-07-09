2e839cabdeb96b8d9fd7083336a49bb146bde70a
Lou Grossi <grossi.lou@gmail.com>
2022-07-08 21:34:05 -0700

    hopping sausage, fixing grass

--- src/assets/grass.ts
+++ src/assets/grass.ts
@@ -2,10 +2,8 @@
     Put grass on the ground
  */
 export default function summonGrass() {
-    //console.log('executing summonGrass()'); 
     var div = document.createElement('div');
     div.setAttribute('id', 'grass');
-    div.style.cssText = 'background-repeat:repeat-x;height:5%;left:0;position:fixed;bottom:0;width:100%;background-image:url("/assets/img/grass.png");background-size:10%';
+    div.style.cssText = 'background-repeat:repeat-x;height:5%;left:0;position:fixed;bottom:0;width:100%;background-image:url("./img/grass.png");background-size:10%;z-index:-1;';
     document.body.appendChild(div);
-
 }
--- src/assets/sausages.ts
+++ src/assets/sausages.ts
@@ -1,7 +1,23 @@
+function followMouse() {
+    $(document).mousemove(function (e) {
+        // happy sausages
+        $("#sausage").css({
+            left: e.pageX,
+            bottom: Math.random() * 30,
+        });
+    });
+}
+
 export function summonSausage1() {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.innerText = '$(document).mousemove(function (e) {$("#sausage").css({left: e.pageX, bottom: Math.random() * 30})});';
+    document.getElementsByTagName('head')[0].appendChild(script);
     var img = document.createElement('img');
     img.setAttribute('id', 'sausage');
-    img.setAttribute('src', '/assets/img/sausage1.png')
-    img.style.cssText = 'height: 30%; position: absolute;';
+    img.setAttribute('src', '/assets/img/sausage1.png');
+    img.style.cssText = 'height: 30%;position:absolute;';
     document.body.appendChild(img);
 }
+
+
