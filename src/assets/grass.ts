5bf8832e89f09fceb1165d9827d2f69b6834b9af
Lou Grossi <grossi.lou@gmail.com>
2022-07-10 09:16:44 -0700

    getting ready to flip the sausage

--- src/assets/grass.ts
+++ src/assets/grass.ts
@@ -2,8 +2,9 @@
     Put grass on the ground
  */
 export default function summonGrass() {
-    var div = document.createElement('div');
-    div.setAttribute('id', 'grass');
-    div.style.cssText = 'background-repeat:repeat-x;height:5%;left:0;position:fixed;bottom:0;width:100%;background-image:url("./assets/img/grass.png");background-size:10%;z-index:100;';
-    document.body.appendChild(div);
+  var div = document.createElement("div");
+  div.setAttribute("id", "grass");
+  div.style.cssText =
+    'background-position:bottom;background-repeat:repeat-x;height:10%;left:0;position:fixed;bottom:0;width:100%;background-image:url("./assets/img/grass.png");background-size:10%;z-index:100;';
+  document.body.appendChild(div);
 }
--- src/assets/sausages.ts
+++ src/assets/sausages.ts
@@ -1,23 +1,47 @@
-function followMouse() {
-    $(document).mousemove(function (e) {
-        // happy sausages
-        $("#sausage").css({
-            left: e.pageX,
-            bottom: Math.random() * 30,
-        });
-    });
+function mouseDirection() {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.innerText =
+    'var direction="",xPrevious=0,mousemovemethod=function(e){if(e.pageX<xPrevious){direction="left"}else if(e.pageX>xPrevious){direction="right"}console.log(direction);xPrevious=e.pageX};document.addEventListener("mousemove",mousemovemethod);';
+  //    'var direction="",xPrevious=0,mousemovemethod=function(e){if(e.pageX<xPrevious){document.getElementById("sausage").style.cssText="-moz-transform:scale(-1, 1);-webkit-transform:scale(1, 1);-o-transform:scale(1, 1);transform:scale(1, 1);filter: FlipH;"}else if(e.pageX>xPrevious){document.getElementById("sausage").style.cssText="-moz-transform:scale(-1, 1);-webkit-transform:scale(-1, 1);-o-transform:scale(-1, 1);transform:scale(-1, 1);filter: FlipH;"}console.log(direction);xPrevious=e.pageX};document.addEventListener("mousemove",mousemovemethod);';
+  //    'var direction="",xPrevious=0,mousemovemethod=function(e){if(e.pageX<xPrevious){document.getElementById("sausage").style.cssText="left: e.pageX; bottom: (Math.random() * window.screen.availHeight) / 25});-moz-transform:scale(-1, 1);-webkit-transform:scale(1, 1);-o-transform:scale(1, 1);transform:scale(1, 1);filter: FlipH;"}else if(e.pageX>xPrevious){document.getElementById("sausage").style.cssText="left: e.pageX; bottom: (Math.random() * window.screen.availHeight) / 25});-moz-transform:scale(-1, 1);-webkit-transform:scale(-1, 1);-o-transform:scale(-1, 1);transform:scale(-1, 1);filter: FlipH;"}console.log(direction);xPrevious=e.pageX};document.addEventListener("mousemove",mousemovemethod);';
+  document.getElementsByTagName("head")[0].appendChild(script);
+}
+
+function hoppingSausage() {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.innerText =
+    '$(document).mousemove(function (e) {$("#sausage").css({left: e.pageX, bottom: (Math.random() * window.screen.availHeight) / 25})});';
+  document.getElementsByTagName("head")[0].appendChild(script);
+}
+
+// TODO: create a parameter that allows loading multiple sausages
+function displaySausage() {
+  var img = document.createElement("img");
+  img.setAttribute("id", "sausage");
+  img.setAttribute("src", "/assets/img/sausage1.png");
+  img.style.cssText = "height:30%;position:absolute;";
+  document.body.appendChild(img);
 }
 
 export function summonSausage1() {
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.innerText = '$(document).mousemove(function (e) {$("#sausage").css({left: e.pageX, bottom: Math.random() * 30})});';
-    document.getElementsByTagName('head')[0].appendChild(script);
-    var img = document.createElement('img');
-    img.setAttribute('id', 'sausage');
-    img.setAttribute('src', '/assets/img/sausage1.png');
-    img.style.cssText = 'height: 30%;position:absolute;';
-    document.body.appendChild(img);
+  hoppingSausage();
+  mouseDirection();
+  displaySausage();
 }
 
-
+// var direction = "",
+//     xPrevious = 0,
+//     mousemovemethod = function(e) {
+//         if (e.pageX < xPrevious) {
+//             //direction = "left"
+//           document.getElementById('sausage').style.cssText = 'left: e.pageX; bottom: (Math.random() * window.screen.availHeight) / 25});-moz-transform:scale(-1, 1);-webkit-transform:scale(1, 1);-o-transform:scale(1, 1);transform:scale(1, 1);filter: FlipH;';
+//         } else if (e.pageX > xPrevious) {
+//             //direction = "right"
+//           document.getElementById('sausage').style.cssText = 'left: e.pageX; bottom: (Math.random() * window.screen.availHeight) / 25});-moz-transform:scale(-1, 1);-webkit-transform:scale(-1, 1);-o-transform:scale(-1, 1);transform:scale(-1, 1);filter: FlipH;';
+//         }
+//         console.log(direction);
+//         xPrevious = e.pageX
+//     };
+// document.addEventListener("mousemove", mousemovemethod);
