88266d8adb00ec88d16e11df3b1e752eedca2d5b
Lou Grossi <grossi.lou@gmail.com>
2022-07-11 10:24:44 -0700

    not functional yet

--- src/assets/sausages.ts
+++ src/assets/sausages.ts
@@ -1,20 +1,21 @@
-function mouseDirection() {
-  var script = document.createElement("script");
-  script.type = "text/javascript";
-  script.innerText =
-    '$(document).mousemove(function (e) {$("#sausage").css({left: e.pageX, bottom: (Math.random() * window.screen.availHeight) / 25; })});-moz-transform:scale(1, 1);-webkit-transform:scale(1, 1);-o-transform:scale(1, 1);filter: FlipH;';
-
-  //  'var direction="",xPrevious=0,mousemovemethod=function(e){if(e.pageX<xPrevious){direction="left"}else if(e.pageX>xPrevious){direction="right"}console.log(direction);xPrevious=e.pageX};document.addEventListener("mousemove",mousemovemethod);';
-  //    'var direction="",xPrevious=0,mousemovemethod=function(e){if(e.pageX<xPrevious){document.getElementById("sausage").style.cssText="-moz-transform:scale(-1, 1);-webkit-transform:scale(1, 1);-o-transform:scale(1, 1);transform:scale(1, 1);filter: FlipH;"}else if(e.pageX>xPrevious){document.getElementById("sausage").style.cssText="-moz-transform:scale(-1, 1);-webkit-transform:scale(-1, 1);-o-transform:scale(-1, 1);transform:scale(-1, 1);filter: FlipH;"}console.log(direction);xPrevious=e.pageX};document.addEventListener("mousemove",mousemovemethod);';
-  //    'var direction="",xPrevious=0,mousemovemethod=function(e){if(e.pageX<xPrevious){document.getElementById("sausage").style.cssText="left: e.pageX; bottom: (Math.random() * window.screen.availHeight) / 25});-moz-transform:scale(-1, 1);-webkit-transform:scale(1, 1);-o-transform:scale(1, 1);transform:scale(1, 1);filter: FlipH;"}else if(e.pageX>xPrevious){document.getElementById("sausage").style.cssText="left: e.pageX; bottom: (Math.random() * window.screen.availHeight) / 25});-moz-transform:scale(-1, 1);-webkit-transform:scale(-1, 1);-o-transform:scale(-1, 1);transform:scale(-1, 1);filter: FlipH;"}console.log(direction);xPrevious=e.pageX};document.addEventListener("mousemove",mousemovemethod);';
-  document.getElementsByTagName("head")[0].appendChild(script);
-}
+// function mouseDirection() {
+//   var script = document.createElement("script");
+//   script.type = "text/javascript";
+//   script.innerText =
+//     // 'var xPrevious=0,css="",mousemovemethod=function(e){if(e.pageX<xPrevious){css="left: e.pageX; bottom: (' +
+//     // Math.random() * window.screen.availHeight +
+//     // ')/25;-moz-transform:scale(1, 1);-webkit-transform:scale(1, 1);-o-transform:scale(1, 1);transform: scale(1, 1);filter:FlipH;"}else if(e.pageX>xPrevious){css="left: e.pageX; bottom: (' +
+//     // Math.random() * window.screen.availHeight +
+//     // ')/25;-moz-transform:scale(1, 1);-webkit-transform:scale(-1, 1);-o-transform:scale(-1, 1);transform: scale(-1, 1);filter:FlipH;"}document.getElementById("sausage").style.cssText=css;console.log(css)};document.addEventListener("mousemove",mousemovemethod);';
+//     'var direction="",xPrevious=0,mousemovemethod=function(e){if(e.pageX<xPrevious){console.log("left");}else if(e.pageX>xPrevious){console.log("right");};document.addEventListener("mousemove",mousemovemethod)};';
+//   document.getElementsByTagName("head")[0].appendChild(script);
+// }
 
 function hoppingSausage() {
   var script = document.createElement("script");
   script.type = "text/javascript";
   script.innerText =
-    '$(document).mousemove(function (e) {$("#sausage").css({left: e.pageX, bottom: (Math.random() * window.screen.availHeight) / 25})});';
+    'var xPrevious=0;document.mousemovemethod=function(e){if(e.pageX<xPrevious){$("#sausage").css({left:e.pageX,bottom:(Math.random()*window.screen.availHeight)/25});console.log("left")}else if(e.pageX>xPrevious){$("#sausage").css({left:e.pageX,bottom:(Math.random()*window.screen.availHeight)/25});console.log("right");}document.addEventListener("mousemove",mousemovemethod););';
   document.getElementsByTagName("head")[0].appendChild(script);
 }
 
@@ -23,27 +24,29 @@ function displaySausage() {
   var img = document.createElement("img");
   img.setAttribute("id", "sausage");
   img.setAttribute("src", "/assets/img/sausage1.png");
-  img.style.cssText = "height:30%;position:absolute;";
+  //img.style.cssText = "height:30%;position:absolute;";
   document.body.appendChild(img);
 }
 
 export function summonSausage1() {
-  hoppingSausage();
-  mouseDirection();
   displaySausage();
+  hoppingSausage();
 }
 
-// var direction = "",
-//     xPrevious = 0,
-//     mousemovemethod = function(e) {
-//         if (e.pageX < xPrevious) {
-//             //direction = "left"
-//           document.getElementById('sausage').style.cssText = 'left: e.pageX; bottom: (Math.random() * window.screen.availHeight) / 25});-moz-transform:scale(-1, 1);-webkit-transform:scale(1, 1);-o-transform:scale(1, 1);transform:scale(1, 1);filter: FlipH;';
-//         } else if (e.pageX > xPrevious) {
-//             //direction = "right"
-//           document.getElementById('sausage').style.cssText = 'left: e.pageX; bottom: (Math.random() * window.screen.availHeight) / 25});-moz-transform:scale(-1, 1);-webkit-transform:scale(-1, 1);-o-transform:scale(-1, 1);transform:scale(-1, 1);filter: FlipH;';
-//         }
-//         console.log(direction);
-//         xPrevious = e.pageX
-//     };
-// document.addEventListener("mousemove", mousemovemethod);
+var xPrevious = 0;
+document.mousemovemethod = function (e) {
+  if (e.pageX < xPrevious) {
+    $("#sausage").css({
+      left: e.pageX,
+      bottom: (Math.random() * window.screen.availHeight) / 25,
+    });
+    console.log("left");
+  } else if (e.pageX > xPrevious) {
+    $("#sausage").css({
+      left: e.pageX,
+      bottom: (Math.random() * window.screen.availHeight) / 25,
+    });
+    console.log("right");
+  }
+  document.addEventListener("mousemove", mousemovemethod);
+};
