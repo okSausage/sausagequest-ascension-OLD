d1b00b5e6e62043a69c393c2934a42ee0b5e5b9c
someguynamedlou <grossi.lou@icloud.com>
2023-06-27 23:51:11 -0400

    refactor of grass renderer

--- src/assets/grass.ts
+++ src/assets/grass.ts
@@ -1,10 +1,21 @@
-/*
-    Put grass on the ground
- */
 export default function summonGrass() {
-  var div = document.createElement("div");
-  div.setAttribute("id", "grass");
-  div.style.cssText =
-    'background-position:bottom;background-repeat:repeat-x;height:10%;left:0;position:fixed;bottom:0;width:100%;background-image:url("./assets/img/grass.png");background-size:10%;z-index:100;';
-  document.body.appendChild(div);
+  const grassId = "grass";
+
+  if (!document.getElementById(grassId)) {
+    const grassDiv = document.createElement("div");
+    grassDiv.id = grassId;
+    grassDiv.style.cssText = `
+      background-position: bottom;
+      background-repeat: repeat-x;
+      height: 10%;
+      left: 0;
+      position: fixed;
+      bottom: 0;
+      width: 100%;
+      background-image: url("./assets/img/grass.png");
+      background-size: 10%;
+      z-index: 100;
+    `;
+    document.body.appendChild(grassDiv);
+  }
 }
